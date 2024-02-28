ea4f503faf31a37b7026d82683685c8c74f6c54b
= <=>
2024-02-27 23:12:19 -0500

    Se creo la estructura mvc para el usuario al 10%

---
+++ GestionTareas/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using GestionTareas.Dato;
+using GestionTareas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionTareas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        UsuarioAdmin admin = new UsuarioAdmin();
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            IEnumerable<usuarios> lista = admin.Consultar();
+            return View(lista);
+        }
+    }
+}
---
+++ GestionTareas/Dato/UsuarioAdmin.cs
@@ -0,0 +1,20 @@
+﻿using GestionTareas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionTareas.Dato
+{
+    public class UsuarioAdmin
+    {
+        public IEnumerable<usuarios> Consultar()
+        {   
+            
+            using (BD_TAREASEntities contexto = new BD_TAREASEntities())
+            {
+                return contexto.usuarios.AsNoTracking().ToList();
+            }
+        }
+    }
+}
